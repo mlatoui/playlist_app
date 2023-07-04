4f107813345fe13a70ccc590ba57f58bbf680998
mourad <mourad.latoui.89@gmail.com>
2023-07-04 16:20:26 -0400

    addition of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import "./App.css";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { About } from './pages/about';
+import { Home } from './pages/home';
+import PlayList from './components/PlayList';
 
 function App() {
   return (
-    <div className="App">
-      <div id="header-container">
-        <Header />
-        <Navbar />
+    <Router>
+      <div className="App">
+        <div id="header-container">
+          <Header />
+          <Navbar />
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/about" element={<PlayList />} />
+          </Routes>
+        </div>{' '}
+        <Footer />
       </div>
-      <div className="content">
-        <Main />
-      </div>{" "}
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
